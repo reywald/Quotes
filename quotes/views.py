ea436ee77bdfe43dd16ec8319aa134d5296ed8d6
Agbarakwe Ikechukwu <iaweepingsaint@gmail.com>
2021-03-18 10:56:02 +0100

    Viewing records with Quote ListView template

--- quotes/views.py
+++ quotes/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from django.views.generic.list import ListView
 
 from .models import Quote
 from .forms import QuoteForm
 from pages.models import Page
+
+
+class QuoteList(ListView):
+    model = Quote
+    context_object_name = 'all_quotes'
+
+    def get_context_data(self, **kwargs):
+        context = super(QuoteList, self).get_context_data(**kwargs)
+        context['page_list'] = Page.objects.all()
+        return context
 
 
 def quote_req(request):
